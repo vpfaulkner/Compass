65c9211eeb7c350726f0030239f16cc16d7f7c6a
vpfaulkner@gmail.com <vpfaulkner@gmail.com>
2014-11-17 16:47:17 -0500

    Added dummy json response to #search

--- app/controllers/api/v1/legislators_controller.rb
+++ app/controllers/api/v1/legislators_controller.rb
@@ -2,7 +2,8 @@ class API::V1::LegislatorsController < ActionController::API
   include ActionController::HttpAuthentication::Token::ControllerMethods
 
   def search
-    render json: {message: 'Resource not found'}
+    my_json = { "legislators" => [{"first_name" => "John","last_name" => "Doe","role" => "Senator","State" => "NC","Party" => "Democrat","ID" => 1}, {"first_name" => "Jane","last_name" => "Smith","role" => "Representative","State" => "NC","Party" => "Republican","ID" => 2} ] }
+    render json: my_json
   end
 
 end
